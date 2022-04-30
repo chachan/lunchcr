d083d6919f8e714e46a2596c8dd93ae1978d9417
Cherny D. C. Berbesí I <cherny.berbesi@protonmail.com>
2022-04-30 03:45:33 -0600

    feat: use config file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import argparse
-import pathlib
+import configparser
 import os
+import pathlib
 
 from lunchable import LunchMoney
 
@@ -9,16 +10,22 @@ from entities.bac import BACAccount
 ENTITIES = [BACAccount]
 
 
-def main(datapath):
-    files = os.listdir(datapath)
-    print(files)
-    # file_name = ""
+def main(datapath, config):
+    access_token = config["lunchmoney"].get("access_token")
+    lunch_money = LunchMoney(access_token)
+    files = [each for each in os.listdir(datapath) if each.endswith(".csv")]
+    if not files:
+        print(f"could not find csv files in {datapath}")
+
     # entity = [entity.infer(file_name) for entity in ENTITIES]
 
 
 if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read("config.cfg")
+
     parser = argparse.ArgumentParser()
     parser.add_argument("datapath", type=pathlib.Path)
     args = parser.parse_args()
 
-    main(args.datapath)
+    main(args.datapath, config)
